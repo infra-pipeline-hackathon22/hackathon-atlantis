eea39bd30d2bd7ab107c6c4815cacc484275c080
Tom Butler <tom.b@canva.com>
2022-07-21 14:38:53 +1000

    roles

---
+++ terraform/roles/roles.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "atlantis_role" {
+  source = "../modules/atlantis-role"
+
+  role = {
+    name = "atlantis"
+    namespace = "atlantis"
+    service_accounts = "atlantis"
+  }
+}
+
+data "aws_iam_policy_document" "test_readwrite" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "test_readwrite" {
+  source = "../modules/eng-role"
+  role_name = "test-readwrite"
+  role_policy = data.aws_iam_policy_document.test_readwrite.json
+  atlantis_role_arn = module.atlantis_role.role.arn
+}
+
+output "atlantis" {
+  value = module.atlantis_role.role.arn
+}
