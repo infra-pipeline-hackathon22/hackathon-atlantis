23a51ae4a0c3b65fead6dddac97c626f296d5d32
Tom Butler <tom.b@canva.com>
2022-07-21 14:40:08 +1000

    roles

---
+++ terraform/modules/atlantis-role/main.tf
@@ -0,0 +1,50 @@
+variable "role" {
+  type = object({
+    name = string
+    namespace = string
+    service_accounts = string
+  })
+
+  validation {
+    condition = length(var.role.service_accounts) > 0
+    error_message = "The role.service_accounts value must have at least one element."
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect = "Allow"
+    principals {
+      identifiers = ["arn:aws:iam::240508968475:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/87C3022853A908387D967229798BD666"]
+      type        = "Federated"
+    }
+    condition {
+      test = "StringEquals"
+      values = ["system:serviceaccount:${var.role["namespace"]}:${var.role.service_accounts}"]
+      variable = "oidc.eks.us-west-2.amazonaws.com/id/87C3022853A908387D967229798BD666:sub"
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name = var.role.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "role_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    resources = ["arn:aws:iam::240508968475:role/atlantis/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "policy" {
+  policy = data.aws_iam_policy_document.role_policy.json
+  role   = aws_iam_role.role.id
+}
+
+output "role" {
+  value = aws_iam_role.role
+}
---
+++ terraform/modules/eng-role/main.tf
@@ -0,0 +1,33 @@
+variable "role_name" {
+  type = string
+}
+
+variable "role_policy" {
+  type = string
+}
+
+variable "atlantis_role_arn" {
+  type = string
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect = "Allow"
+    principals {
+      identifiers = [var.atlantis_role_arn]
+      type        = "AWS"
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  path = "/atlantis/"
+}
+
+resource "aws_iam_role_policy" "policy" {
+  policy = var.role_policy
+  role   = aws_iam_role.role.id
+}
