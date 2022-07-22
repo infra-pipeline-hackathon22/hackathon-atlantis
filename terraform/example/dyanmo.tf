cf55a6bbcb9acab16aedc480e34c023b214b9d10
Marc Hildenbrand <mhilde@canva.com>
2022-07-22 05:51:21 +0000

    Test atlantis with dynamo apply

---
+++ terraform/example/dyanmo.tf
@@ -0,0 +1,10 @@
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "Backend"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "User"
+  attribute {
+    name = "User"
+    type = "S"
+  }
+}
