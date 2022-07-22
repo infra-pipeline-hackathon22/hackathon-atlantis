c9786f92189f946a8726d427090501f8b1cd8a06
Tom Butler <tom.b@canva.com>
2022-07-22 11:35:56 +1000

    remote state

--- terraform/modules/atlantis-role/main.tf
+++ terraform/modules/atlantis-role/main.tf
@@ -34,14 +34,48 @@ resource "aws_iam_role" "role" {
 
 data "aws_iam_policy_document" "role_policy" {
   statement {
-    effect = "Allow"
-    actions = ["sts:AssumeRole"]
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
     resources = ["arn:aws:iam::240508968475:role/atlantis/*"]
   }
 }
 
 resource "aws_iam_role_policy" "policy" {
   policy = data.aws_iam_policy_document.role_policy.json
+  role   = aws_iam_role.role.id
+}
+
+data "aws_iam_policy_document" "terraform_remote_state" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::atlantis-hackathon-terraform-state"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:GetObjectVersion",
+      "s3:GetObjectVersionAcl",
+      "s3:DeleteObjectVersion",
+    ]
+    resources = ["arn:aws:s3:::atlantis-hackathon-terraform-state/*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/terraform-lock"]
+  }
+}
+
+resource "aws_iam_role_policy" "terraform_remote_state_policy" {
+  policy = data.aws_iam_policy_document.terraform_remote_state.json
   role   = aws_iam_role.role.id
 }
 
--- terraform/modules/eng-role/main.tf
+++ terraform/modules/eng-role/main.tf
@@ -27,7 +27,41 @@ resource "aws_iam_role" "role" {
   path = "/atlantis/"
 }
 
+data "aws_iam_policy_document" "terraform_remote_state" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::atlantis-hackathon-terraform-state"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:GetObjectVersion",
+      "s3:GetObjectVersionAcl",
+      "s3:DeleteObjectVersion",
+    ]
+    resources = ["arn:aws:s3:::atlantis-hackathon-terraform-state/*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/terraform-lock"]
+  }
+}
+
 resource "aws_iam_role_policy" "policy" {
+  policy = data.aws_iam_policy_document.terraform_remote_state.json
+  role   = aws_iam_role.role.id
+}
+
+resource "aws_iam_role_policy" "terraform_remote_state_policy" {
   policy = var.role_policy
   role   = aws_iam_role.role.id
 }
